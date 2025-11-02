de0c8edfa20e58c0810019a58514beb5b67ee2c1
Javohir Hoshimov <javohirobbo@gmail.com>
2025-11-02 16:41:31 +0500

    Update ALLOWED_HOSTS for production settings

--- watchhouse_project/settings.py
+++ watchhouse_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@l%9+m(xkp4y=^bplx3--vyxptj=vw-dgp972ruhr_#)@luj73
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['watchhouse-5ahm.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
